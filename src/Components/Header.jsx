a9e67de7d3096b047b4e6a78b3230c008f664ef9
Shubham Naiknaware <62801488+shubhamnaiknaware@users.noreply.github.com>
2024-04-06 10:08:26 +0530

    Adding name to Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,26 +1,25 @@
+export default function getUser() {
+  const user = {
+    name: "shubham",
+    lastName: "naiknaware",
+    fullName: function () {
+      return this.name + " " + this.lastName;
+    },
+  };
+  const day = 10;
 
-export default function getUser(){
-    const user = {
-        name : "shubham",
-        lastName: "naiknaware",
-        fullName : function(){
-            return this.name  + " "+ this.lastName;
-        }
-    }
-    const day = 10;
+  let val = "name";
 
-    let val = "name";
-    
-    val = val ? "shubham" : "Tom";
+  val = val ? "shubham" : "Tom";
 
-    const weather = 'cold'
-    
-    return (
-        <diV>
-            <h1>{ user.fullName()}</h1>
-            <h1>{day}</h1>
-            <h1>{weather + " " +  val}</h1>
-        </diV>
-    )
+  const weather = "cold";
+
+  return (
+    <diV>
+      <h1>{user.fullName()}</h1>
+      <h1>{day}</h1>
+      <h1>{weather + " " + val}</h1>
+      <h1>{"name"}</h1>
+    </diV>
+  );
 }
-
